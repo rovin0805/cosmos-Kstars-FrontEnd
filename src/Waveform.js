953bea5b527bf94af4ced21f573abaf2360ddb76
rayoungji <dudwl7676@naver.com>
2020-02-05 21:57:56 +0900

    구간재생구현

--- src/Waveform.js
+++ src/Waveform.js
@@ -2,6 +2,11 @@ import React from 'react';
 import WaveSurfer from 'wavesurfer';
 
 class Waveform extends React.Component {
+  state = {
+    zoomPixel: 10,
+    startTime: 0.0,
+    endTime: 0.0
+  }
   componentDidMount() {
     const aud = document.querySelector('#song');
 
@@ -22,8 +27,66 @@ class Waveform extends React.Component {
   }
 
   playIt = () => {
-    this.wavesurfer.playPause();
+    this.wavesurfer.play();
   };
+
+  Pause = () => {
+    this.wavesurfer.pause();
+  }
+
+  ZoomIn =() => {
+
+    this.setState({
+      zoomPixel: this.state.zoomPixel+10,
+    });
+
+
+    this.wavesurfer.zoom(this.state.zoomPixel);
+
+  }
+
+  ZoomOut =() => {
+
+    if(this.state.zoomPixel>0){
+      this.setState({
+        zoomPixel: this.state.zoomPixel-10,
+      });
+    }
+
+
+    this.wavesurfer.zoom(this.state.zoomPixel);
+
+  }
+
+  GetCurrentTimeForStart=()=>{
+   
+    this.setState({
+      startTime: this.wavesurfer.getCurrentTime()
+    })
+  }
+
+  GetCurrentTimeForEnd=()=>{
+   
+    this.setState({
+      endTime: this.wavesurfer.getCurrentTime()
+    })
+  }
+
+  Distroy=()=>{
+    this.wavesurfer.destroy();
+  }
+
+  PlayIng = () => {
+
+    this.wavesurfer.play(this.state.startTime,this.state.endTime);
+  }
+
+  Destroy = () =>{
+    this.wavesurfer.on('pause', function () {
+      this.wavesurfer.destroy();
+  });
+  
+  }
 
 //https://taehongdev.github.io/html_css_study/exam03/audio/The_Weeknd-I_Feel_It_Coming(cover_byJ.Fla).mp3
 //https://reelcrafter-east.s3.amazonaws.com/aux/test.m4a
@@ -31,13 +94,22 @@ class Waveform extends React.Component {
     return (
       <div>
         <button onClick={this.playIt}>Play</button>
+        <button onClick={this.Pause}>Stop</button>
+        
+        <button onClick={this.ZoomIn}>+</button>
+        <button onClick={this.ZoomOut}>-</button>
+        <button onClick={this.GetCurrentTimeForStart}>구간선택(시작)</button>
+        <button onClick={this.GetCurrentTimeForEnd}>구간선택(끝)</button>
+        <button onClick={this.PlayIng}>선택구간재생</button>
+        <button onClick={this.destroy}>선택구간삭제</button>
+
         <div
           style={{ border: '1px solid grey', width: 900, height: 80 }}
           id="waveform"
         />
         <audio
           id="song"
-          src="https://taehongdev.github.io/html_css_study/exam03/audio/The_Weeknd-I_Feel_It_Coming(cover_byJ.Fla).mp3"
+          src="https://reelcrafter-east.s3.amazonaws.com/aux/test.m4a"
         />
       </div>
     );
