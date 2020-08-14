1998c679b34d408ae4d10331093e8cb8a4b8308a
rayoungji <dudwl7676@naver.com>
2020-08-14 20:25:42 +0900

    헤더 라우트 추가

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react'
 import {Header, Body, Footer} from "./component/layout/index";
+import { BrowserRouter, Route } from "react-router-dom";
+
 
 class App extends Component {
   render() {
     return (
       <div>
-        <Header/>
+        {/* <Header/> */}
+        <Route exact path="/main" component={Header} /> {/*메인화면*/}
         <Body/>
         <Footer/>
       </div>
